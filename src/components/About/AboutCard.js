a6d0bad9d9d22efaa1cc8563bc02fedbd6262738
yuvam2017 <dryuvam14900@gmail.com>
2022-07-25 02:59:03 +0530

    firstcommit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">D R YUVAM </span>
+            from <span className="purple"> New Delhi, India.</span>
+            <br />I am a junior pursuing Honours in Computer Science at University of Delhi.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Watching Movies
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Reading Books
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
